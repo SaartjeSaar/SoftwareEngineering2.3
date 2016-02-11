ac3b05a0070d64afad8cb3c00af8e03b2f959da3
cnlu95 <caro.lu@hotmail.com>
2016-02-11 14:24:25 +0100

    Dobbelsteen klaar!

--- src/DobbelsteenMVC.java
+++ src/DobbelsteenMVC.java
@@ -6,11 +6,12 @@ public class DobbelsteenMVC extends JApplet
 	DobbelsteenModel model;             //het model
 	TekstView tekstView;              // view
 	DobbelsteenView dobbelsteenView;  // view
+	GridView gridView;				//view	
 	DobbelsteenController controller;             // Controller
 	
 	public void init()
 	{
-		resize(250,200);
+		resize(500,400);
         
 		// Maak het model
 		model = new DobbelsteenModel();
@@ -27,10 +28,14 @@ public class DobbelsteenMVC extends JApplet
         tekstView = new TekstView();
         tekstView.setBackground(Color.green);
         getContentPane().add(tekstView,BorderLayout.SOUTH);
+        gridView = new GridView ();
+        gridView.setBackground(Color.white);
+        getContentPane().add(gridView,BorderLayout.EAST);
         
         // Registreer de views bij het model
         model.addActionListener(tekstView);
         model.addActionListener(dobbelsteenView);
+        model.addActionListener(gridView);
         
         // Eerste worp
         model.gooi();
