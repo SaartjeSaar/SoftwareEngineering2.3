3b9214ed3ac23f118fd004c3d04eb0dec1a271e2
SaartjeSaar <sarafailly@msn.com>
2016-02-11 15:12:30 +0100

    Opdracht10

---
+++ src/Opdracht10/Array.java
@@ -0,0 +1,15 @@
+package Opdracht10;
+import java.util.Random;
+
+
+public class Array {
+
+	public static void main(String[] args) {
+		int array1[];
+		int array2[];
+		
+	Random randomGenerator = new Random();
+
+	}
+
+}
