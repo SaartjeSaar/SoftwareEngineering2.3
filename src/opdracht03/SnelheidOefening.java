d4fe4f647f4d4bf9fce53c9a567cfcc336b65df1
cnlu95 <caro.lu@hotmail.com>
2016-02-18 11:36:15 +0100

    update.

--- src/opdracht03/SnelheidOefening.java
+++ src/opdracht03/SnelheidOefening.java
@@ -1,17 +1,25 @@
 package opdracht03;
+import java.util.Random;
 
-public class SnelheidOefening {
+import junit.framework.TestCase;
+
+public class SnelheidOefening extends TestCase {
 
 	/**
 	 * @param args
 	 */
 	public static void main( String[] args){
 		GetalRij gr = new GetalRij( 100000, 200000);
+		Random random = new Random();
+		//(range) + minimum
+		int zoekWaarde = random.nextInt(100001) + 100000;
+		gr.zitErinA(zoekWaarde);		
+		
 	}
 
+	
 	// Hulpmethode voor tijdsbepaling
 	private static long tijd(){
 		return System.currentTimeMillis();
 	}
-
 }
